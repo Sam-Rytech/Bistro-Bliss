7251fc6811f4598056953a7d48e7c372b9850889
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-26 22:22:15 +0100

    updated import statement

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -2,6 +2,7 @@ import Footer from '@/components/Home/Footer'
 import Navbar from '@/components/Home/Navbar'
 import MenuPage from '@/components/Menu/MenuPage'
 import MenuFilter from '@/components/Menu/MenuFilter'
+import MenuCard from '@/components/Menu/MenuCard'
 import React from 'react'
 
 const Menu = () => {
@@ -11,6 +12,7 @@ const Menu = () => {
       <Footer />
       <MenuPage />
       <MenuFilter />
+      <MenuCard />
     </div>
   )
 }
