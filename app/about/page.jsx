6c70cbf92a3689e124a3e73a1e7c4131b8b2cf13
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-26 17:36:44 +0100

    updated the Navbar import statement

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/About/Navbar";
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/About/Footer";
 import AboutHero from "@/components/About/AboutHero";
 import VideoSection from "@/components/About/VideoSection";
