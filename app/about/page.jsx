aa8abe07838c91685f0e9c736ae1d048a60e8d1b
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-26 04:12:55 +0100

    Updated import statement

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,11 +1,11 @@
 import Navbar from "@/components/About/Navbar";
-import Footer from "../(components)/Footer";
-import AboutHero from "../(components)/AboutHero";
-import VideoSection from "../(components)/VideoSection";
-import Features from "../(components)/Features";
-import AboutInfo from "../(components)/AboutInfo";
-import Stats from "../(components)/Stats";
-import Testimonials from "../(components)/Testimonials";
+import Footer from "@/components/About/Footer";
+import AboutHero from "@/components/About/AboutHero";
+import VideoSection from "@/components/About/VideoSection";
+import Features from "@/components/About/Features";
+import AboutInfo from "@/components/About/AboutInfo";
+import Stats from "@/components/About/Stats";
+import Testimonials from "@/components/About/Testimonials";
 
 export default function AboutPage() {
   return (
