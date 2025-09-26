08d6fd907455e47d1ccb3e816a7532dc66f64858
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-26 01:46:24 +0100

    updated the Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,56 +1,27 @@
 "use client"
-import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'
-import Companies from '@/components/Companies'
-import Commitments from '@/components/Commitments'
-import HowItWorks from '@/components/HowItWorks'
-import OurWork from '@/components/OurWork'
-import CTASection from '@/components/CTASection'
-import Team from '@/components/Team'
-import Testimonials from '@/components/Testimonials'
-import FAQ from '@/components/FAQ'
-import FinalCTA from '@/components/FinalCTA'
-import Footer from '@/components/Footer'
-
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Menu from "@/components/Menu";
+import About from "@/components/About";
+import Services from "@/components/Services";
+import Delivery from "@/components/Delivery";
+import Testimonials from "@/components/Testimonials";
+import Blog from "@/components/Blog";
+import Footer from "@/components/Footer";
+import { use } from "react";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col min-h-screen">
-      {/* Navbar */}
+    <main className="font-sans">
       <Navbar />
-
-      {/* Hero Section */}
       <Hero />
-
-      {/* Companies Section */}
-      <Companies />
-
-      {/* Commitments Section */}
-      <Commitments />
-
-      {/* How It Works Section */}
-      <HowItWorks />
-
-      {/* Our Work Section */}
-      <OurWork />
-
-      {/* CTA Section */}
-      <CTASection />
-
-      {/* Team Section */}
-      <Team />
-
-      {/* Testimonials Section */}
+      <Menu />
+      <About />
+      <Services />
+      <Delivery />
       <Testimonials />
-
-      {/* FAQ Section */}
-      <FAQ />
-
-      {/* Final CTA Section */}
-      <FinalCTA />
-
-      {/* Footer */}
+      <Blog />
       <Footer />
     </main>
-  )
+  );
 }
