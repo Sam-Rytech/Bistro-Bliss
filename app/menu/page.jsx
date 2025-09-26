ad288660adbbd936464db430e539623e00ffc367
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-26 21:28:30 +0100

    updated the menu page

---
+++ app/menu/page.jsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/Home/Footer'
+import Navbar from '@/components/Home/Navbar'
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default Menu
