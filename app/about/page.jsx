516f09ed87d43b68bc19ed0f222723ad428a6038
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-26 04:03:31 +0100

    Updated the About page

---
+++ app/about/page.jsx
@@ -0,0 +1,23 @@
+import Navbar from "../(components)/Navbar";
+import Footer from "../(components)/Footer";
+import AboutHero from "../(components)/AboutHero";
+import VideoSection from "../(components)/VideoSection";
+import Features from "../(components)/Features";
+import AboutInfo from "../(components)/AboutInfo";
+import Stats from "../(components)/Stats";
+import Testimonials from "../(components)/Testimonials";
+
+export default function AboutPage() {
+  return (
+    <main className="font-sans">
+      <Navbar />
+      <AboutHero />
+      <VideoSection />
+      <Features />
+      <AboutInfo />
+      <Stats />
+      <Testimonials />
+      <Footer />
+    </main>
+  );
+}
